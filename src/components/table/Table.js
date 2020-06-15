7290dcaab0637320d452c138e1473e43c3ed5910
Vladimir Degtyarev <vladimir1860921@gmail.com>
2020-06-15 15:23:08 +0300

    Finish table resize

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,11 +1,27 @@
-import {ExcelComponent} from "@core/ExcelComponent";
-import {createTable} from "@/components/table/table.template";
+import {ExcelComponent} from '@core/ExcelComponent'
+import {createTable} from '@/components/table/table.template'
+import {resizeHandler} from "@/components/table/table.resize";
+import {shouldResize} from "@/components/table/table.function";
 
 export class Table extends ExcelComponent {
-
     static className = 'excel__table';
+
+    constructor($root) {
+        super($root, {
+            listeners: ['mousedown']
+        })
+    }
 
     toHTML() {
         return createTable(20);
     }
+
+    onMousedown(event) {
+        if (shouldResize(event)) {
+            resizeHandler(this.$root, event);
+        }
+    }
 }
+
+// 252, 143, -> 116 ms scripting
+// 2177, 1783, -> 199 ms rendering
