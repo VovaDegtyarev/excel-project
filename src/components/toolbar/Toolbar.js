8db6c0df4b41ec8c01d464a463ca1ef4e2550e35
Vladimir Degtyarev <vladimir1860921@gmail.com>
2020-06-16 19:36:34 +0300

    finish base table logic

--- src/components/toolbar/Toolbar.js
+++ src/components/toolbar/Toolbar.js
@@ -1,39 +1,45 @@
-import {ExcelComponent} from "@core/ExcelComponent";
+import {ExcelComponent} from '@core/ExcelComponent'
 
-export class Toolbar extends ExcelComponent{
+export class Toolbar extends ExcelComponent {
+  static className = 'excel__toolbar'
 
-    static className = 'excel__toolbar';
+  constructor($root, options) {
+    super($root, {
+      name: 'Toolbar',
+      listeners: ['click'],
+      ...options
+    })
+  }
 
-    constructor($root) {
-        super($root, {
-            name: 'Toolbar',
-            listeners: ['click']
-        });
-    }
+  toHTML() {
+    return `
+      <div class="button">
+        <i class="material-icons">format_align_left</i>
+      </div>
 
-    toHTML() {
-        return `<div class="button">
-                <i class="material-icons">format_align_left</i>
-            </div>
-            <div class="button">
-                <i class="material-icons">format_align_center</i>
-            </div>
-            <div class="button">
-                <i class="material-icons">format_align_right</i>
-            </div>
-            <div class="button">
-                <i class="material-icons">format_bold</i>
-            </div>
-            <div class="button">
-                <i class="material-icons">format_italic</i>
-            </div>
-            <div class="button">
-                <i class="material-icons">format_underlined</i>
-            </div>`;
-    }
+      <div class="button">
+        <i class="material-icons">format_align_center</i>
+      </div>
 
-    onClick(event) {
-        console.log(event.target);
-    }
+      <div class="button">
+        <i class="material-icons">format_align_right</i>
+      </div>
 
+      <div class="button">
+        <i class="material-icons">format_bold</i>
+      </div>
+
+      <div class="button">
+        <i class="material-icons">format_italic</i>
+      </div>
+
+      <div class="button">
+        <i class="material-icons">format_underlined</i>
+      </div>
+    `
+  }
+
+  onClick(event) {
+    console.log(event.target)
+  }
 }
--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -1,23 +1,44 @@
-import {DomListener} from "@core/DomListener";
+import {DomListener} from '@core/DomListener'
 
 export class ExcelComponent extends DomListener {
+  constructor($root, options = {}) {
+    super($root, options.listeners)
+    this.name = options.name || ''
+    this.emitter = options.emitter
+    this.unsubscribers = []
 
-    constructor($root, options = {}) {
-        super($root, options.listeners);
-        this.name = options.name || '';
-    }
+    this.prepare()
+  }
 
-    //возвращает шаблон компонента
-    toHTML() {
-        return '';
-    }
+  // Настраивааем наш компонент до init
+  prepare() {}
 
-    init() {
-        this.initDOMListener();
-    }
+  // Возвращает шаблон компонента
+  toHTML() {
+    return ''
+  }
 
-    destroy() {
-        this.removeDOMListener();
-    }
+  // Уведомляем слушателей про событие event
+  $emit(event, ...args) {
+    this.emitter.emit(event, ...args)
+  }
 
+  // Подписываемся на событие event
+  $on(event, fn) {
+    const unsub = this.emitter.subscribe(event, fn)
+    this.unsubscribers.push(unsub)
+  }
+
+  // Инициализируем компонент
+  // Добавляем DOM слушателей
+  init() {
+    this.initDOMListeners()
+  }
+
+  // Удаляем компонент
+  // Чистим слушатели
+  destroy() {
+    this.removeDOMListeners()
+    this.unsubscribers.forEach(unsub => unsub())
+  }
 }
